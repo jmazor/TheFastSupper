375a7b7beb340f26d8a8d9a12cabf58250e5c653
Milton Joseph <miltonjoseph138@gmail.com>
2023-03-18 01:50:18 -0400

    Revert "Created separate file for HomePage"
    This reverts commit d36f577e50652a7e08dbb9691c1d2c65bb8a594a.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-import HomePage from './pages/HomePage';
 class App extends Component {
   render() {
     return (
-     <HomePage />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>The Fast Supper</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
