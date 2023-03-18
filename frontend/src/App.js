b0e68621acc7e3455ac5d8153676ec2279a2a3aa
Milton Joseph <miltonjoseph138@gmail.com>
2023-03-18 05:14:43 -0400

    added Log in + updated env to new database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './custom.css';
 import
   {
     Button,
@@ -19,7 +20,10 @@ import axios from 'axios';
 const App = () =>
 {
   const [modal, setModal] = useState(false);
+  const [loginModal, setLoginModal] = useState(false);
   const toggle = () => setModal(!modal);
+  const toggleLogin = () => setLoginModal(!loginModal);
+
 
   const handleSubmit = async (e) =>
   {
@@ -45,42 +49,113 @@ const App = () =>
     toggle();
   };
 
+  const handleLogin = async (e) =>
+  {
+    e.preventDefault();
+    const data = {
+      email: e.target.loginEmail.value,
+      password: e.target.loginPassword.value,
+    };
+
+    // Make the API request to your backend to log in the user (update the URL accordingly)
+    try
+    {
+      const response = await axios.post('https://fastsupper.herokuapp.com/api/login', data);
+      console.log(response.data);
+    } catch (error)
+    {
+      console.error('Error:', error);
+    }
+
+    // Close the modal after submitting the form
+    toggleLogin();
+  };
+
   return (
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>The Fast Supper</h2>
       </div>
-      <div className="App-intro">
-        <Button color="primary" onClick={toggle}>
+
+      <div className="App-body">
+        {/*Sign up Button*/}
+        <Button className="custom-button" color="primary" onClick={toggle}>
           Sign Up
         </Button>
+        {/* Login in Button*/}
+        <Button className="custom-button login-button" color="primary" onClick={toggleLogin}>
+          Log In
+        </Button>
 
+
+        {/*Modal for login, contains form elements login, password */}
+        <Modal isOpen={loginModal} toggle={toggleLogin}>
+          <Form onSubmit={handleLogin}>
+
+          <ModalHeader toggle={toggleLogin}>Log In</ModalHeader>
+          <ModalBody>
+            
+            <FormGroup>
+              <Label for="loginEmail">Email</Label>
+              <Input type="email" name="loginEmail" id="loginEmail" required />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="loginPassword">Password</Label>
+              <Input type="password" name="loginPassword" id="loginPassword" required />
+            </FormGroup>
+          </ModalBody>
+          
+          <ModalFooter>
+              
+              <Button color="primary" type="submit">
+                Log In
+              </Button>
+              
+              <Button color="secondary" onClick={toggleLogin}>
+                Cancel
+              </Button>          
+              
+              </ModalFooter>
+          </Form>
+        </Modal>
+
+        {/*Modal for sign up */}
         <Modal isOpen={modal} toggle={toggle}>
           <Form onSubmit={handleSubmit}>
             <ModalHeader toggle={toggle}>Sign Up</ModalHeader>
             <ModalBody>
+              
+              {/*Contains form elements first name, last name, email, password */}
               <FormGroup>
                 <Label for="firstName">First Name</Label>
                 <Input type="text" name="firstName" id="firstName" required />
               </FormGroup>
+
               <FormGroup>
                 <Label for="lastName">Last Name</Label>
                 <Input type="text" name="lastName" id="lastName" required />
               </FormGroup>
+
               <FormGroup>
                 <Label for="email">Email</Label>
                 <Input type="email" name="email" id="email" required />
               </FormGroup>
+
               <FormGroup>
                 <Label for="password">Password</Label>
                 <Input type="password" name="password" id="password" required />
               </FormGroup>
+
             </ModalBody>
+
+
             <ModalFooter>
               <Button color="primary" type="submit">
                 Sign Up
               </Button>
+
               <Button color="secondary" onClick={toggle}>
                 Cancel
               </Button>
