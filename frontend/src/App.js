f4cf271eb97a94e3a7e22f6c8296b390ed0c55b7
Milton Joseph <miltonjoseph138@gmail.com>
2023-03-18 01:50:09 -0400

    Revert "changes app from class to function"
    This reverts commit bd138dd6b8e0e8beef97f87fcd93bca11b334e31.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 import HomePage from './pages/Homepage';
-function App() {
-
+class App extends Component {
+  render() {
     return (
      <HomePage />
     );
-  
+  }
 }
 
 export default App;
