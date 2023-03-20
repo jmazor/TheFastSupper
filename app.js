63e5f45e5098be8ea18cce7cf136980bad29a904
jmazor <mazorj@pm.me>
2023-03-19 21:08:36 -0400

    added phony restaurant api

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Sets up an Express app
 const { express, bodyParser } = require('./modules');
 const authRouter = require('./api/auth');
+const userRouter = require('./api/user');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(authRouter);
+app.use(userRouter);
 
 module.exports = app;
