5e436c7db627d9f6807665e01698335ef8904ae6
jevans556 <47763236+jevans556@users.noreply.github.com>
2023-03-19 20:53:44 -0400

    Get rid of react logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,7 +74,6 @@ const App = () =>
   return (
     <div className="App">
       <div className="App-header">
-        //<img src={logo} className="App-logo" alt="logo" />
         <h2>The Fast Supper</h2>
       </div>
 
