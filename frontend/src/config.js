dcce5839ca1a654ae7c917c09356e7f2822a697a
jevans556 <47763236+jevans556@users.noreply.github.com>
2023-03-20 21:59:26 -0400

    Fix URL path

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@ import React from "react";
 
 const config = {
     url: process.env.NODE_ENV ==='production' ?
-    'https://fastsupper.herokuapp.com/' : 'http://localhost:5000/'
+    'https://fastsupper.herokuapp.com' : 'http://localhost:5000'
 }
 export default config;
