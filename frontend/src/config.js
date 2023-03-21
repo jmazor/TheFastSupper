d9746ed4dca1cd6264bd8eae5f693fd2ce3c143b
jevans556 <47763236+jevans556@users.noreply.github.com>
2023-03-20 20:23:43 -0400

    fix config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const config = {
-    url: process.env.NODE_ENV ==='Production' ?
+    url: process.env.NODE_ENV ==='production' ?
     'https://fastsupper.herokuapp.com/' : 'http://localhost:5000/'
 }
 export default config;
