7eaf14cc3680b4514888775a7806e987c8800dfa
jmazor <mazorj@pm.me>
2023-03-20 22:06:39 -0400

    Removed Cross Origin from production

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const userRouter = require('./api/user');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+  }
   
 
 app.use(authRouter);
