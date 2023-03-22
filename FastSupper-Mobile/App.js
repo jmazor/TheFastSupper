622ee2ac2aef3cfa3d72e7d1f08634f55d10c8e1
samuelbauta <89614131+samuelbauta@users.noreply.github.com>
2023-03-22 18:00:36 -0400

    new mobile screens

--- FastSupper-Mobile/App.js
+++ FastSupper-Mobile/App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import react from 'react';
+//tell React that we will implement a navigator
+import { NavigationContainer } from "@react-navigation/native";
+//create a stack navigator
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./components/homescreen";
+import LoginScreen from "./components/loginscreen";
+import SignupScreen from "./components/signupscreen";
+
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Anthony is sexy AF!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignupScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Anthony is sexy AF!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ FastSupper-Mobile/components/loginscreen.js
@@ -0,0 +1,11 @@
+// loginscreen.js
+import React, { Component } from "react";
+import { Button, View, Text } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
