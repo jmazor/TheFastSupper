25a3ca2e42f2ff6b74eaf8d3ef3375ac41c9a14b
jevans556 <47763236+jevans556@users.noreply.github.com>
2023-03-29 22:33:10 -0400

    Added searching for restaurants

---
+++ frontend/src/components/CollaspeComponent.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import '../App.css';
+import '../custom.css';
+import { useNavigate } from "react-router-dom";
+import
+  {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Collapse,
+    CardBody,
+    Card
+  } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CollapseComponent = (props) =>{
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const { restaurants } = props
+
+    return (
+        <div>
+            {restaurants.name}
+            <Button color="primary" onClick={toggle} style={{ marginBottom: '0.5rem', marginLeft: '1rem', marginTop: '0.5rem' }}>
+                Show More
+            </Button>
+            <Collapse isOpen={isOpen} >
+                <Card>
+                    <CardBody>
+                        cost: {restaurants.price} <br/>
+                        Address: {restaurants.address}, {restaurants.city}, {restaurants.state} {restaurants.zipCode}<br />
+                        Rating: {restaurants.rating} Stars<br />
+                        phone : {restaurants.phone} <br />
+                        link :  <a href={restaurants.restURL}>Visit Us</a> <br />
+                        <img src={restaurants.imageURL} class="restaurantImg"/>
+                    </CardBody>
+                </Card>
+            </Collapse>
+        </div >
+    )
+
+}
+export default CollapseComponent
---
+++ frontend/src/components/RestaurantCollaspe.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import '../App.css';
+import '../custom.css';
+import CollapseComponent from './CollaspeComponent';
+import { useNavigate } from "react-router-dom";
+import
+  {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Collapse,
+    CardBody,
+    Card
+  } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function RestaurantCollaspe(props) {
+    const DisplayRestaurants = (props) => {
+        const [isOpen, setIsOpen] = useState(false);
+        const toggle = () => setIsOpen(!isOpen);
+        const { restaurants } = props
+        let styles = {
+            display: 'inline-block'
+        }
+        if (restaurants.length > 0) {
+            return (
+                restaurants.map((restaurant, index) => {
+                    return (
+                        <CollapseComponent restaurants = {restaurant} />
+                    )
+                })
+            )
+        }
+        else {
+            return (
+                <div>
+                    <h1>Lets find your favorite restaurants</h1>
+                </div>
+            )
+        }
+    }
+    return (
+        <div>
+            {DisplayRestaurants(props)}
+        </div>
+    )
+}
+export default RestaurantCollaspe;
